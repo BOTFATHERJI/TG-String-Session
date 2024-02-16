96b54f72e257c14160278501e5c3cfc6c3aedf1d
BOTFATHERJI <82192896+BOTFATHERJI@users.noreply.github.com>
2024-02-16 13:45:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import os
 
 from heroku3 import from_key
-from pyrogram import Client
-from pyromod import listen
+from telethon import Client
+from telemod import listen
 
 API_ID = int(os.environ.get("API_ID", 0))
 API_HASH = os.environ.get("API_HASH", None)
